c7a80ddfef6b5b0116b9696c1d2876658bc6f625
vitorTest <vytorfelix2@gmail.com>
2022-07-12 18:15:27 -0300

    apply proportions and measures

--- 02-gofinancez/src/screens/Dashboard/index.tsx
+++ 02-gofinancez/src/screens/Dashboard/index.tsx
@@ -1,11 +1,38 @@
 import React from 'react';
+import { PushNotificationIOS } from 'react-native';
 
-import { Container, Title } from './styles';
+import { 
+  Container,
+  Header,
+  UserWrapper,
+  UserInfo,
+  User,
+  Photo,
+  UserGreeting,
+  Username
+} from './styles';
 
 export function Dashboard() {
   return (
     <Container>
-      <Title>Dashboard</Title>
+      <Header>
+        <UserWrapper>
+          <UserInfo>
+            <Photo 
+              source={{ uri: 'https://avatars.githubusercontent.com/u/10060108?v=4'}}
+            />
+            <User>
+              <UserGreeting>
+                Olá,
+              </UserGreeting>
+              <Username>
+                Vitor
+              </Username>
+            </User>
+
+          </UserInfo>
+        </UserWrapper>
+      </Header>
     </Container>
   )
 }
